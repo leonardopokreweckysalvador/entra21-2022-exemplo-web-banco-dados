5037ebe6be12872e593f9401edec49738dfa06e4
leonardo.salvador <leonardo.pokrewecky@gmail.com>
2022-08-03 22:06:01 -0300

    Final da aula

---
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
+{
+    public class RacaController : Controller
+    {
+        [Route("/raca")]
+        [HttpGet]
+
+        /// Endpoist que  permite listar todas as raças
+        /// <returns> Retorna a página hyml com as raças </returns>>
+        public IActionResult ObterTodos()
+        {
+            return View("Index");
+        }
+
+        [Route("/raca/cadastrar")]
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [Route("/raca/registrar")]
+        [HttpGet]
+
+        public IActionResult Registrar(
+            [FromQuery]string nome,
+            [FromQuery] string especie)
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
